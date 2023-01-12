ffa386c8286fcc15ff2f3b5eb8883b09c68f6206
Saidislombek Abdusamatov <111279907+Saidislombek-dev@users.noreply.github.com>
2023-01-12 21:37:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ if file:
     st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}%')
 
     # plotting
-    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    fig = px.bar("class"=probs*100, "prediction"=model.dls.vocab)
     st.plotly_chart(fig)
